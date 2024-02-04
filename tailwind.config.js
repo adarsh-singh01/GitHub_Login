1ac6b705a367c6b9fb914a5bfad70b701c2f1863
Your Name <as8936940798@gmail.com>
2024-02-04 14:27:08 +0530

    complete clone

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {colors:{
+      "git-body":"#0D1117",
+      "git-box":"#161B22",
+      "btn0":"#238636",
+      "btn1":"#2B983F",
+      'borderC':'#30363D',
+      'fontC':"#E6EDF3",
+      'outlineC':'#2F7DED'
+    },
+  screens:{
+    'xxsm':'251px',
+    'xsm':'330px',
+
+  }},
+  },
+  plugins: [],
+}
+
